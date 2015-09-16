6e5e2ab750a211703e56f74262281dd7c5f2dc7e
warlyware <danward@gmail.com>
2015-09-16 14:33:56 -0400

    Add animation, visibility of overlay-content based on setTimeout

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,2 +1,13 @@
-console.log('loaded!');
-console.log('another thing');
+var overlay = document.getElementById('overlay');
+var overlayContent = document.getElementById('overlay-content');
+
+setTimeout(function() {
+  overlay.classList.remove('hidden');
+  overlayContent.classList.add('overlay-animate');
+  overlayContent.classList.remove('hidden');
+}, 3000);
+
+setTimeout(function() {
+  overlayContent.classList.add('hidden');
+  overlay.classList.add('hidden');
+}, 5000);
